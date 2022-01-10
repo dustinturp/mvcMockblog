3409b1eecf11c19e9b642891220b31fd5e1e35c0
dustinturp <dustinturp@outlook.com>
2022-01-09 20:41:08 -0600

    working on bug fixes.

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 //cookie session
 const sess = {
   secret: 'Super Secret secret', //saved in env
@@ -30,9 +29,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // built in middlware function
+app.use(express.static(path.join(__dirname, 'public'))); // built in middleware function
 // turn on routes // app.use(routes);
 app.use(require('./controllers/'));
+
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
